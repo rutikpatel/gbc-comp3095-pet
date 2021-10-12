f0b3bdcead30f58a541762824e993fd6bd548b89
Rutik Patel <rutikpatel3660@gmail.com>
2021-10-11 20:53:14 -0400

    first commit

---
+++ gbc-comp3095-pet/petclinic/src/test/java/ca/gbc/gbccomp3095pet/GbcComp3095PetApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.gbccomp3095pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbcComp3095PetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
